c784d1a71e9de7f6c1296e3eedca6f5fb28ddb71
JoanVicens <38570230+JoanVicens@users.noreply.github.com>
2019-10-27 00:48:23 +0200

    ds

--- js/prevision-bases.js
+++ js/prevision-bases.js
@@ -1,11 +1,11 @@
 const PONER = 'poner';
 const QUITAR = 'quitar';
-function crear_prevision(tipo, id, nombre) {
+function crear_prevision(tipo, id) {
     let prevision = document.createElement('div');
     prevision.classList.add('bonificacion');
     prevision.classList.add(tipo);
 
-    prevision.innerHTML = `${id}: ${nombre}`;
+    prevision.innerHTML = `${id}`;
 
     document.getElementById('previsiones').appendChild(prevision);
 
@@ -21,9 +21,9 @@ function crear_prevision(tipo, id, nombre) {
                  prevision = response.aggregations.by_id.buckets;
                  prevision.forEach((base)=>{
                     if(base.avg_porcentaje_ocupacion.value <= 0.1) {
-                        crear_prevision(PONER, base);
+                        crear_prevision(PONER, base.key);
                     } else if (base.avg_porcentaje_ocupacion.value >= 0.9) {
-                        crear_prevision(QUITAR, base);
+                        crear_prevision(QUITAR, base.key);
                     }
                 });
             },
