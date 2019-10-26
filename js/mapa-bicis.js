c2d6eba4130b3866aea78ad9e11653ad30c69ebc
JoanVicens <38570230+JoanVicens@users.noreply.github.com>
2019-10-26 21:05:39 +0200

    mostrar bases

--- js/mapa-bicis.js
+++ js/mapa-bicis.js
@@ -3,12 +3,35 @@ let ejunt = [39.987556,-0.0468827];
 let graph = document.getElementById('BaseGraph');
 let btn_cerrar = document.getElementById('cerrar_g');
 
+let marcadores_bases = [];
+
 btn_cerrar.addEventListener('click', ()=> {
     graph.classList.remove('mostrar');
     graph.classList.add('amagar');
 })
 
+function anyadir_marcadores(data) {
+    let bases = data.hits.hits;
 
+    bases.forEach((base) => {
+
+        if(base.porcentaje_ocupacion < 0.1) {
+            var color = 'orange';
+        } else if(base.porcentaje_ocupacion > 0.9) {
+            var color = 'blue';
+        }
+
+        let circle = L.circle(ejunt, {
+        	color,
+        	fillColor: '#fdfdfd',
+        	fillOpacity: 0.5,
+        	radius: 25
+        }).addTo(mymap).on("click", function (e) {
+            showBaseGraph();
+        });
+        marcadores_bases.push(circle);
+    })
+}
 function showBaseGraph() {
 graph.classList.remove('amagar');
 graph.classList.add('mostrar');
@@ -20,10 +43,10 @@ console.log(graph.childNodes[2]);
 $.ajax({ url: 'controladores/mapas.php?action=getBasesExistentes',
          type: 'POST',
          dataType: 'json',
-         success: function(response) { 
-            console.log(response);
+         success: function(response) {
+            anyadir_marcadores(response);
         },
-        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
             console.log(errorThrown)
         }
 });
